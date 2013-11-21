28fe2c0400cd1c8c70f0c20b6b5b2841385e2916
Adrian Roy Baguio <bgdr0015@humbermail.ca>
2013-11-21 00:53:59 -0500

    Updated log.php

--- log.php
+++ log.php
@@ -2,6 +2,13 @@
 /********************************
 @author Adrian Roy Antonio Baguio
 @date 20/11/2013
+@description Files has been stored
+in repository.
+********************************/
+
+/********************************
+@author Adrian Roy Antonio Baguio
+@date 20/11/2013
 @description Registration page added.
 With form validation.
 ********************************/
